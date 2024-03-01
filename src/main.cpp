c5ce6292082cc92c29302f8f257dd77d93f14eda
emrealpar21 <emrealpar21@gmail.com>
2024-03-02 01:45:37 +0300

    Fixed some cmake error on windows but not completed

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include <spdlog/spdlog.h>
 #include <boost/asio.hpp>
 
-#include "server.hpp"
+//#include "server.hpp"
 
 int main() {
-    boost::asio::io_context ctx;
-    boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address_v4::any(),1235);
-    Server s(ctx,ep);
-    s.start();
-    spdlog::info("Hello world");
-    ctx.run();
+//    boost::asio::io_context ctx;
+//    boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address_v4::any(),1235);
+//    Server s(ctx,ep);
+//    s.start();
+      spdlog::info("Hello world");
+//    ctx.run();
 }
